215fa44aad137ae0a0c4dbd1e4b45b4127ad1ef7
place4code <place4code@gmail.com>
2018-09-25 13:40:19 +0200

    First commit TEST

---
+++ easy-gallery/easy-gallery.js
@@ -0,0 +1,38 @@
+"use strict";
+
+jQuery.fn.easygallery = function() { 
+    //this = .gallery-item
+    
+    $(document).keydown(function(event) {
+        if (event.key == "Backspace" || event.key == "Escape") {
+            $("#easy-gallery-overlay").remove();
+            $("#easy-gallery-overlay-image-container").remove();
+        }
+    });
+
+    $(this).click(function(event) { //this = .gallery-item
+
+        event.preventDefault();
+        
+        let overlay = $.parseHTML("<div id='easy-gallery-overlay'></div>");
+        
+        $("body").append(overlay);
+
+        let image = $.parseHTML("<img id='easy-gallery-overlay-image'>");
+        let href = $(this).attr("href");
+        $(image).attr("src", href);
+
+        let container = $.parseHTML("<div id='easy-gallery-overlay-image-container'></div>");
+        $(container).append(image);
+
+        $(container).click(function () {
+            $("#easy-gallery-overlay").remove();
+            $("#easy-gallery-overlay-image-container").remove();
+        });
+
+        $("body").append(container);
+        
+    });
+
+    return this;
+}
---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+jQuery(document).ready(function ($) {
+
+    $(".easy-gallery-gallery-item").easygallery();
+
+});
